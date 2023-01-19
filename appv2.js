230b29c0bc5c84eab4ea5e90686c6034b0f6d0a7
magagr17 <markjohnaguirre@gmail.com>
2023-01-19 12:03:31 +0800

    successfully fetched images, titles, author name and other needed data in open-library API

--- appv2.js
+++ appv2.js
@@ -1,51 +1,57 @@
-// const search = document.querySelector(".searchbox");
+const search = document.querySelector(".searchbox");
 
-
-// search.addEventListener("keyup", async (e) => {
-//   e.preventDefault();
-//   const searchTerm = e.target.value.toLowerCase();
-//   const config = { params: { q: searchTerm } };
-// //   // const res = await axios.get(`http://openlibrary.org/search.json?q=${searchTerm}`) or
-//   const res = await axios.get(`http://openlibrary.org/search.json`,config);
+const searchBook = () => {
+    search.addEventListener("keyup", async (e) => {
+  e.preventDefault();
+  const searchTerm = e.target.value.toLowerCase();
+  const config = { params: { q: searchTerm } };
+//   // const res = await axios.get(`http://openlibrary.org/search.json?q=${searchTerm}`) or
+  const res = await axios.get(`http://openlibrary.org/search.json`,config);
  
-//   console.log(res.data.docs);
-  
+    let output = '';
 
-//   for (i = 0; (i = res.data.items.length); i++) {
-//     const resultSearch = res.data.items[i].volumeInfo;
-//     console.log(resultSearch);
+    res.data.docs.forEach(book => {
+
+        var bookTitle = book.title;
+        if (book.title.length >= 40){
+            bookTitle = book.title.slice(0,40);
+        } 
+
+        var bookAuthor = book.author_name
+        if (book.author_name.length >1 ){
+             bookTitle = book.author_name[0]
+        }
+
+        var pubYear = book.publish_year;
+        if (book.publish_year.length > 1) {
+            pubYear = book.publish_year[0]
+        }
+
+        var imageSrc = "https://covers.openlibrary.org/b/id/"+ book.cover_i + "-L.jpg";
+        if (book.cover_i === undefined){
+            imageSrc = "https://bimp-eaga.asia/sites/default/files/styles/cover/public/default_images/no-cover.png?itok=QYKv5ZlY"
+        }
+
+        output +=
+            `<div class="productContent">
+    <div class="imageContainer">
+        <img class="productImg" src = ${imageSrc} alt=""
+        />
+        </div>
+        <span class="bookTitle">${bookTitle}</span>
+    <div class='lowerProduct'>
+        <span class="author">${bookAuthor}</span>
+        <span class="publishedYear">${pubYear}</span>
+        </div>
+    </div>`
+
+      })
+
+      productContainer.innerHTML = output;
     
-//   }
-
-//   //create a function to show results
-//   const displayProducts = (filteredProducts) => {
-//     //filteredProducts is just an argument sample
-
-//       productContainer.innerHTML = filteredProducts.map(
-//           (item) =>
-//             `<div class="productContent">
-//     <div class="imageContainer">
-//         <img class='productImg'
-//         src = ${item.imageLinks.thumbnail}
-//         alt=""
-//         />
-//         </div>
-//         <span class="bookTitle">${item.title}</span>
-//     <div class='lowerProduct'>
-//         <span class="author"> ${item.author[0]}</span>
-//         <span class="publishedYear">&#8369;${item.publishedDate.splice(
-//           0,
-//           4
-//         )}</span>
-//         </div>
-//     </div>`
-//         )
-//         .join("");
-//   };
-//   displayProducts(resultSearch);
-// });
-
-
+    })
+} 
+searchBook();
 
 
 //create function to showAll
@@ -61,30 +67,60 @@
 
 // create a function to show results
 
+
 const productContainer = document.querySelector(".products");
 
 const displayProducts = async () => {
 
-    const randomLetter = Math.random().toString(36).slice(2, 4);
+    const randomLetter = Math.random().toString(36).slice(2, 3);
+     //random letter/number search to generate random result
+
     const config = { params: { title: randomLetter } };
-    const res = await axios.get(`http://openlibrary.org/search.json`,config);
+    const res = await axios.get(`http://openlibrary.org/search.json`, config);
     console.log(res.data.docs);
-    productContainer.innerHTML = res.data.docs.map((item) => {
+
+    let output = '';
+    
+    
+    res.data.docs.slice(0,-10).forEach(book => {    // res.data.docs is an array of object 
+        //sliced 10 only
+        
+        var bookTitle = book.title;
+        if (book.title.length >= 40){
+            bookTitle = book.title.slice(0,40);
+        } 
+
+        var bookAuthor = book.author_name
+        if (book.author_name.length > 1 ){
+             bookTitle = book.author_name[0];
+        }
+
+        var pubYear = book.publish_year;
+        if (book.publish_year.length > 1) {
+            pubYear = book.publish_year[0]
+        }
+
+        var imageSrc = "https://covers.openlibrary.org/b/id/"+ book.cover_i + "-L.jpg";
+        if (book.cover_i === undefined){
+            return;
+        }
+
+        output +=
             `<div class="productContent">
     <div class="imageContainer">
-        <img class='productImg'
-        src = 
-        alt=""
+        <img class="productImg" src = ${imageSrc} alt=""
         />
         </div>
-        <span class="bookTitle">${item.title}</span>
+        <span class="bookTitle">${bookTitle}</span>
     <div class='lowerProduct'>
-        <span class="author">${item.author_name[0]}</span>
-        <span class="publishedYear">${item.publish_year[0]}</span>
+        <span class="author">${bookAuthor}</span>
+        <span class="publishedYear">${pubYear}</span>
         </div>
     </div>`
-      })
-        .join("");
 
+      })
+
+      productContainer.innerHTML = output;
+    
     }
   displayProducts();
