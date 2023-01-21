570d37563ad7427fc8f5b72d32f8df218921af5e
magagr17 <markjohnaguirre@gmail.com>
2023-01-21 09:51:25 +0800

    polishing of API integration

--- appv2.js
+++ appv2.js
@@ -4,7 +4,7 @@ const submit = document.querySelector(".magnifyButton");
 const searchInput = document.querySelector(".searchbox");
 const productContainer = document.querySelector(".products");
 const searchText = document.querySelector('.searchText');
-const apiKey = 'AIzaSyAFQozXqx3l-O0Rf6YygcVvrz-COYbYP10';
+const apiKey = 'AIzaSyAozjA5x184ufKIKnTVj3X7I1TlH4yR6gc';
 const trending = document.querySelector('.trending');
 const popularAuthor = document.querySelector('.popularAuthor');
 const trendingContainer = document.querySelector('.products2');
@@ -28,6 +28,7 @@ submit.addEventListener("click", async (e) => {
   popularAuthor.textContent = ''
   popularAuthorContainer.style.display = 'none';
   trendingContainer.style.display = 'none';
+  newContainer.style.display = 'none';
   const res = await axios.get(
     "https://www.googleapis.com/books/v1/volumes?q=" +
       searchTerm +
@@ -49,13 +50,14 @@ submit.addEventListener("click", async (e) => {
 
 const fiction = document.querySelector(".fiction");
 
-fiction.addEventListener("click", async () => {
   
+  fiction.addEventListener("click", async () => {
   searchText.textContent = 'Fiction'
   trending.textContent = ''
   popularAuthor.textContent = ''
   popularAuthorContainer.style.display = 'none';
   trendingContainer.style.display = 'none';
+  newContainer.style.display = 'none';
   const res = await axios.get(
     "https://www.googleapis.com/books/v1/volumes?q=subject:fiction&key=" + apiKey
   );
@@ -71,6 +73,7 @@ children.addEventListener("click", async () => {
   popularAuthor.textContent = ''
   popularAuthorContainer.style.display = 'none';
   trendingContainer.style.display = 'none';
+  newContainer.style.display = 'none';
   const res = await axios.get(
     "https://www.googleapis.com/books/v1/volumes?q=subject:children&key=" + apiKey
   );
@@ -88,6 +91,7 @@ nature.addEventListener("click", async () => {
   popularAuthor.textContent = ''
   popularAuthorContainer.style.display = 'none';
   trendingContainer.style.display = 'none';
+  newContainer.style.display = 'none';
   var res = await axios.get(
     "https://www.googleapis.com/books/v1/volumes?q=subject:nature&key=" + apiKey
   );
@@ -104,6 +108,7 @@ religion.addEventListener("click", async () => {
   popularAuthor.textContent = ''
   popularAuthorContainer.style.display = 'none';
   trendingContainer.style.display = 'none';
+  newContainer.style.display = 'none';
   var res = await axios.get(
     "https://www.googleapis.com/books/v1/volumes?q=subject:religion&key=" + apiKey
   );
@@ -118,31 +123,33 @@ religion.addEventListener("click", async () => {
 //creating range function 
 
 const range = document.querySelector(".rangeBtn");
+const yearValueStart = document.querySelector(".yearValueStart");
 const yearValueEnd = document.querySelector(".yearValueEnd");
 
 range.min = 1900; //inserting innerHTML of range min
 range.max = 2023; //inserting innerHTML of range max
 
 yearValueEnd.textContent = range.max;
+yearValueStart.textContent = range.min;
 range.value = range.max; //setting initial range max to 2023
 
 
-//search function
+//range function
 
-range.addEventListener("input", async (e) => {
+range.addEventListener("click ", async (e) => {
+  e.preventDefault()
   const searchTerm = "a";
   const res = await axios.get(
-    "https://www.googleapis.com/books/v1/volumes?q=" +
-      searchTerm +
-      "&key=" + apiKey
+    "https://www.googleapis.com/books/v1/volumes?q=&key=" + apiKey
   );
 
-  year = e.target.value;
+  year = range.value;
+  console.log(year)
   yearValueEnd.textContent = year;
-  const filteredBook = res.data.items.filter((book) =>{ book.volumeInfo.publishedDate === year
+  const filteredBook = res.data.items.filter((book) =>{ book.volumeInfo.publishedDate <= year
 })
 
-console.log(filteredBook)
+//  console.log(...filteredBook)
 
   
   // totalResult = filteredResult.map((book) => book.volumeInfo.publishedDate )
@@ -212,6 +219,8 @@ const showOutput = () => {
 
 // new released books function
 
+const newContainer = document.querySelector(".products1");
+
 const newReleased = async () => {
   searchText.textContent = 'New Released Books'
     const rand = ('abcdefghijklmnopqrstuvwxyz').split('')[(Math.floor(Math.random() * 26 ))];;
@@ -222,7 +231,60 @@ const newReleased = async () => {
     if (totalResult === undefined){
       return;
     }
-    showOutput();
+    
+    let output = "";
+
+    totalResult.forEach((book) => {
+    // res.data.items is an array of object
+
+    try {
+      var bookTitle = book.volumeInfo.title;
+      if (book.volumeInfo.title >= 40) {
+        bookTitle = book.title.slice(0, 40);
+      }
+
+      var bookAuthor = book.volumeInfo.authors;
+      if (Array.isArray(bookAuthor)) {
+        bookAuthor = book.volumeInfo.authors[0];
+      }
+
+      if (bookAuthor === undefined) {
+        return;
+      }
+
+      var pubYear = book.volumeInfo.publishedDate;
+      if (pubYear.length > 4) {
+        pubYear = pubYear.slice(0, 4);
+      }
+
+      var imageSrc = book.volumeInfo.imageLinks.thumbnail;
+
+      var infoLink = book.volumeInfo.infoLink;
+
+      output += `<div class="productContent">
+    <div class="imageContainer">
+        <img class="productImg" src = ${imageSrc} alt=""
+        />
+        </div>    
+    <div class='lowerProduct'>
+        <span class="bookTitle">${bookTitle}</span>
+        <div class='yearAuthor'>
+        <span class="author">${bookAuthor}</span>
+        <span class="publishedYear">${pubYear}</span>
+        </div>
+        <a class="bookInfo" href=${infoLink}>Book Info</a>
+        </div>
+        
+    </div>`;
+    } catch (e) {
+      return;
+    }
+  }); 
+  newContainer.style.top = '0px'
+  newContainer.innerHTML = output;
+
+
+
 }
 newReleased()
 
@@ -290,7 +352,7 @@ const popularAuthors = async () => {
     }
   });
   popularAuthor.textContent = 'Popular Authors'
-  popularAuthorContainer.style.top = '70px'
+  popularAuthorContainer.style.top = '0px'
   popularAuthorContainer.innerHTML = output;
   
     
@@ -363,7 +425,7 @@ const trendingBooks = async () => {
     return;
   }
 });
-trendingContainer.style.top = '50px';
+trendingContainer.style.top = '0px';
 trendingContainer.innerHTML = output;  
 }
 trendingBooks()
@@ -373,9 +435,5 @@ trendingBooks()
 const forYou = document.querySelector(".home");
 
 forYou.addEventListener("click", async () => {
-  popularAuthor.textContent = 'Popular Authors';
-  trending.textContent = 'Hot Pick For You';
-  newReleased();
-  popularAuthors();
-  trendingBooks();
+  location.reload();
 })
